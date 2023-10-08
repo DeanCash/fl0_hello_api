db04ff8b6d5f83446d1659e405405bdc42d5758d
DeanCash <72015294+DeanCash@users.noreply.github.com>
2023-10-08 15:53:58 +0200

    Added ENV support for the server PORT, and removed some comments

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/DeanCash/fl0_hello_api/routes"
 )
 
-var (
-	host    string = "127.0.0.1"
-	port    int    = 5555
-	address string = net.JoinHostPort(host, fmt.Sprintf("%d", port))
+const (
+	PORT_ENV string = "PORT"
 )
 
-func init() {}
+var (
+	host    string
+	port    string
+	address string
+)
+
+func init() {
+	_port, err := os.LookupEnv(PORT_ENV)
+	if !err {
+		log.Panicln("There is no PORT environment variable!")
+	}
+
+	host = "0.0.0.0"
+	port = _port
+	address = net.JoinHostPort(host, port)
+}
 
 func main() {
 	server := http.NewServeMux()
 
 	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
 	server.HandleFunc("/healthcheck", routes.HealthCheckHandler)
-
-	// .HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.WriteHeader(http.StatusOK)
-
-	// 	w.Write([]byte("Hello, World!"))
-	// })
 
 	log.Println("-- Starting server")
 	if err := http.ListenAndServe(address, server); err != nil {
